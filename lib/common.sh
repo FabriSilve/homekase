ce04736d840b86cd37f513e4d8d548fc41d0404f
Fabrizio Silvestri <fabri.silve.fs@gmail.com>
2026-07-03 00:33:37 +0200

    fix: correctly escape colors

--- lib/common.sh
+++ lib/common.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-RED='\033[0;31m'; YELLOW='\033[1;33m'; GREEN='\033[0;32m'
-BLUE='\033[0;34m'; BOLD='\033[1m'; RESET='\033[0m'
+RED=$'\033[0;31m'; YELLOW=$'\033[1;33m'; GREEN=$'\033[0;32m'
+BLUE=$'\033[0;34m'; BOLD=$'\033[1m'; RESET=$'\033[0m'
 
 info()   { echo -e "${BLUE}ℹ${RESET}  $*"; }
 ok()     { echo -e "${GREEN}✓${RESET}  $*"; }
